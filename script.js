d64690d5a567929157ced0edbee0b11e65238e76
D-Redouane <redouanedaddi316@gmail.com>
2022-04-24 01:15:53 +0100

    chatweb

---
+++ script.js
@@ -0,0 +1,29 @@
+// window.onload=function(){
+
+//     function nameHandler(){
+
+//         if (true){
+//             console.log("hi");
+//             var txt=document.getElementById('txt');
+//             var nameField=document.getElementById('nameField');
+//             var chatarea=document.getElementById('chat-area');
+
+//             nameField.innerHTML=txt.value;
+//             chatarea.innerHTML="";
+//             // nameField.style.backgroundColor='green';
+//             nameField.style.textTransform='capitalize';
+//         }
+//     }
+//     document.addEventListener("keyup",nameHandler);
+// }
+
+
+
+
+
+
+
+
+
+
+
